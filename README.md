3452d3072077854fc9df41aca5e3c4671dd4fb0e
NoahAllan <noahallan33@gmail.com>
2021-05-30 18:45:59 +0100

    Readme update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Balloon 2
 ___
-## Division of Labour:
+### Division of Labour:
 * Back-end:
     * Balloon Software - Corin and Lluc
     * Balloon GPS and Data Sending - Evelyn and Dean
@@ -9,8 +9,24 @@ ___
   * Data Representation - Noah and Finlay
     
 ___
-## Data Being Sent:
+### Data Being Sent:
+* Time
 * Latitude
 * Longitude
 * Altitude
-* 
+  * Pressure
+  * GPS
+* Temperature
+* Pressure
+* Humidity
+* Carbon Dioxide Levels
+* Carbon Monoxide Levels
+* Ozone
+* Nitrous Oxide Levels
+* Oxygen Levels
+* Dust Levels
+* Light Levels
+* UV Light Levels
+
+___
+###  
--- graphs/altitude.py
+++ graphs/altitude.py
@@ -4,19 +4,23 @@ import matplotlib.animation as animation
 from matplotlib import style
 import csv
 import datetime
-style.use('fast')
+style.use('seaborn')
 
 fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
 
 
 def animate(i):
+    line_count = 0
     csvfile = open('C:/Users/noaha/PycharmProjects/balloon-2/test-data.csv', 'r', encoding='utf-8-sig')
     csv_reader = csv.reader(csvfile)
     xs = []
     ys = []
     for line in csv_reader:
-        if len(line) > 1:
+        # if len(line) > 1:
+        if line_count == 0:
+            line_count += 1
+        else:
             x, y = line[3], line[2]
             x1 = datetime.datetime.fromtimestamp(float(x)).strftime('%H:%M:%S')
             xs.append(x1)
